e311ab6e44fbd4241f0087fa536c2625775ea66b
Raton4Laveur <leon.e.windhorst@gmail.com>
2026-04-24 18:37:01 +0200

    updated exploative analysis file

--- R/explorativ.R
+++ R/explorativ.R
@@ -7,33 +7,37 @@ data_analysis |>
 # within subject; anchor vs manipulated
 stat_test_paired <- data_analysis |>
   group_by(group_name) |>
-  wilcox_test(mean_score ~ type, paired = TRUE) |>
+  rstatix::wilcox_test(mean_score ~ type, paired = TRUE) |>
   add_significance()
 
 print(stat_test_paired)
 
-# Levene's Test
+# Levene's Test (typeless)
 subject_data |>
   levene_test(mean_score ~ group_name) |>
   add_significance()
 
+# Levene's Test
 data_analysis |>
   filter(type == "Manipulated") |>
   levene_test(mean_score ~ group_name) |>
   add_significance()
 
-# overall wilcoxon test (including anchor)
-stat_test <- subject_data |>
+# overall wilcoxon test (typeless)
+typeless_stat_test <- subject_data |>
   rstatix::wilcox_test(mean_score ~ group_name) |> # Explicitly use rstatix
   add_significance()
 
-print(stat_test)
+print(typeless_stat_test)
 
 
 # sanity check
 data_analysis |> group_by(group_name, type) |> count()
-
-
 data_analysis |> 
+  pull(pid) |> 
+  n_distinct()
+
+subject_data |> group_by(group_name) |> count()
+subject_data |> 
   pull(pid) |> 
   n_distinct()
