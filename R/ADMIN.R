1e577d35aa206ac9b440b7fbd4dd21dd01318603
Raton4Laveur <leon.e.windhorst@gmail.com>
2026-06-04 11:09:10 +0200

    pipeline fix

--- R/ADMIN.R
+++ R/ADMIN.R
@@ -6,14 +6,14 @@ source(here("R", "read&clean.R"))
 cat("Step 2: Processing demographic summaries & plots...\n")
 source(here("R", "Demographics.R"))
 
-cat("Step 3: Executing primary statistical tests...\n")
+cat("Step 3: Visualizing primary distributions...\n")
+source(here("R", "Visualization.R"))
+
+cat("Step 4: Executing primary statistical tests...\n")
 source(here("R", "Testing.R"))
 
-cat("Step 4: Running exploratory analysis...\n")
+cat("Step 5: Running exploratory analysis...\n")
 source(here("R", "explorativ.R"))
-
-cat("Step 5: Visualizing primary distributions...\n")
-source(here("R", "Visualization.R"))
 
 cat("Step 6: Writing formatted results to Excel...\n")
 source(here("R", "generate_xlsx.R"))
